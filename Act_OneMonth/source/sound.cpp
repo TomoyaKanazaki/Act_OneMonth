4f90eb39ade8c07daadbac35b280104a8acbde64
TomoyaKanazaki <221083@st.yoshida-g.ac.jp>
2024-02-16 10:43:35 +0900

    サウンドを有効化

--- Act_OneMonth/source/sound.cpp
+++ Act_OneMonth/source/sound.cpp
@@ -229,7 +229,6 @@ void CSound::Uninit(void)
 //==========================================
 HRESULT CSound::Play(SOUND_LABEL label)
 {
-	return E_FAIL;
 	XAUDIO2_VOICE_STATE xa2state;
 	XAUDIO2_BUFFER buffer;
 
